3ac5756eb22f12398c322c4ef52e4d4d4f691d78
Habib Shaikh <33100185+habibshaikh14@users.noreply.github.com>
2020-10-31 16:53:35 +0530

    - Add splash scene
    - Add start menu scene
    - Add fonts

---
+++ Garden Defenders/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Configuration variables
+    [SerializeField] float timeToWait = 3f;
+    // State Variables
+    private int currentSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex.Equals(0))
+        {
+            StartCoroutine(LoadStartMenuScene());
+        }
+    }
+
+    private IEnumerator LoadStartMenuScene()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    private void QuitGame() 
+    {
+        Application.Quit();
+    }
+}
